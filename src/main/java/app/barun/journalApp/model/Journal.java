72bb710f0c1b78a3829580af31babaf4701c8c34
Barun Panthi Sharma <barunpanthisharma11@gmail.com>
2024-11-07 20:35:10 +0545

    User entities has been added

--- src/main/java/app/barun/journalApp/model/Journal.java
+++ src/main/java/app/barun/journalApp/model/Journal.java
@@ -1,6 +1,7 @@
 package app.barun.journalApp.model;
 
 import lombok.Data;
+import lombok.NonNull;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -14,6 +15,7 @@ public class Journal {
     @Id
     private ObjectId id;
 
+    @NonNull
     private String title;
 
     private String content;
---
+++ src/main/java/app/barun/journalApp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package app.barun.journalApp.repository;
+
+import app.barun.journalApp.model.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository  extends MongoRepository<User, ObjectId> {
+    User findByUsername(String username);
+}
