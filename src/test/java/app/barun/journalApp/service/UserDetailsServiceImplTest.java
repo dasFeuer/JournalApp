1d5ee01759bc9e09a9b8110a48872d70771b90c3
Barun Panthi Sharma <barunpanthisharma11@gmail.com>
2024-11-16 16:38:58 +0545

    Spring Profiles [dev, prod]

--- src/test/java/app/barun/journalApp/service/UserDetailsServiceImplTest.java
+++ src/test/java/app/barun/journalApp/service/UserDetailsServiceImplTest.java
@@ -15,9 +15,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.test.context.ActiveProfiles;
 
 import java.util.ArrayList;
 
+@ActiveProfiles("dev")
 public class UserDetailsServiceImplTest {
 
     @InjectMocks
