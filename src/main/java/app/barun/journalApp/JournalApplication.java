625d26abf28dee84d7246b2befbcafdd113940d0
Barun Panthi Sharma <barunpanthisharma11@gmail.com>
2024-11-18 08:13:25 +0545

    External Weather API integrated

--- src/main/java/app/barun/journalApp/JournalApplication.java
+++ src/main/java/app/barun/journalApp/JournalApplication.java
@@ -10,6 +10,7 @@ import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.MongoTransactionManager;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableTransactionManagement
@@ -24,6 +25,11 @@ public class JournalApplication {
     @Bean
     public PlatformTransactionManager platformTransactionManager(MongoDatabaseFactory dbFactory){
         return new MongoTransactionManager(dbFactory);
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 
 
---
+++ src/main/java/app/barun/journalApp/service/WeatherService.java
@@ -0,0 +1,27 @@
+package app.barun.journalApp.service;
+
+
+import app.barun.journalApp.api.Response.WeatherResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class WeatherService {
+
+    private static final String apiKey = "90533986c31e1deaa706824b44c687ec";
+    private static final String API = "http://api.weatherstack.com/current?access_key=API_KEY&query=CITY";
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    public WeatherResponse getWeather(String city){
+        String finalApi = API.replace("CITY", city).replace("API_KEY", apiKey);
+        ResponseEntity<WeatherResponse> response = restTemplate.exchange(finalApi, HttpMethod.GET, null, WeatherResponse.class);
+        response.getStatusCode();
+        WeatherResponse body = response.getBody();
+        return body;
+    }
+}
