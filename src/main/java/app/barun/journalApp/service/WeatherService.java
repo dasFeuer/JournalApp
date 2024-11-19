71cb470c9a1ac2b43eed813ce07b69b965a52990
Barun Panthi Sharma <barunpanthisharma11@gmail.com>
2024-11-19 07:44:22 +0545

    Service used instead of Component for readability

--- src/main/java/app/barun/journalApp/service/WeatherService.java
+++ src/main/java/app/barun/journalApp/service/WeatherService.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-@Component
+@Service
 public class WeatherService {
 
     private static final String apiKey = "90533986c31e1deaa706824b44c687ec";
