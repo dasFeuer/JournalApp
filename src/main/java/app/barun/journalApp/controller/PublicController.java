a10ff57b5b2cb75eef7e4b3b5cd13ef387088d6b
Barun Panthi Sharma <barunpanthisharma11@gmail.com>
2024-11-25 08:27:05 +0545

    JWT filter added but need to add more functionality

--- src/main/java/app/barun/journalApp/controller/PublicController.java
+++ src/main/java/app/barun/journalApp/controller/PublicController.java
@@ -1,30 +1,71 @@
 package app.barun.journalApp.controller;
 
 import app.barun.journalApp.entity.User;
+import app.barun.journalApp.service.UserDetailsServiceImpl;
 import app.barun.journalApp.service.UserService;
+import app.barun.journalApp.utilis.JwtUtil;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Slf4j
 @RequestMapping("/public")
 public class PublicController {
 
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserDetailsServiceImpl userDetailsService;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
     @GetMapping("/health-check")
     public String healthCheck() {
         return "OK!";
     }
 
-    @PostMapping("/create-user")
-    public void createUser(@RequestBody User user) {
+    @PostMapping("/signup")
+    public void signup(@RequestBody User user) {
         userService.saveNewUser(user);
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody User user) {
+        try{
+
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            user.getUserName(),
+                            user.getPassword()));
+
+            UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUserName());
+
+
+            String jwt = jwtUtil.generateToken(userDetails.getUsername());
+            return new ResponseEntity<>(jwt, HttpStatus.OK);
+        } catch (Exception e){
+
+            log.error("Exception occurred while create Authentication token ", e);
+            return new ResponseEntity<>("Incorrect username or password", HttpStatus.BAD_REQUEST);
+
+
+        }
+    }
+
 //
-//    @PostMapping("/create-user")
-//    public ResponseEntity<String> createUser(@RequestBody User user) {
+//    @PostMapping("/signup")
+//    public ResponseEntity<String> signup(@RequestBody User user) {
 //        try {
 //            userService.saveNewUser(user);
 //            return new ResponseEntity<>("User created successfully!", HttpStatus.CREATED);
